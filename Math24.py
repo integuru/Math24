70dad46a07c62a79ec38579d1ea77274124b97b3
integuru <integuru@gmail.com>
2016-08-03 15:09:31 -0400

    Update Math24.py
    version 1

--- Math24.py
+++ Math24.py
@@ -1,48 +1,70 @@
-'Return all the numbers that can be evaluated to 24'
+from random import randint
+from os import remove, rename
 
-lastCard = 11   # define largest card value; counting J=10, Q=10, K=10
-#lastCard = 14   # define largest card value; counting J=11, Q=12, K=13
-cardDeck=range(1,lastCard)
-operatorList= ["+", "-", "*", "/"]  # define valid card operators
-# Add precedence to the operations by placing () around a plain formula
-operations = ['    {:>3} {:>2}     {:>3}   {:>2}   {:>3}       {:>2} {:>3}    ',\
-              '  ( {:>3} {:>2}     {:>3} ) {:>2}   {:>3}       {:>2} {:>3}    ',\
-              '    {:>3} {:>2}   ( {:>3}   {:>2}   {:>3} )     {:>2} {:>3}    ',\
-              '    {:>3} {:>2}     {:>3}   {:>2} ( {:>3}       {:>2} {:>3} )  ',\
-              '  ( {:>3} {:>2}     {:>3} ) {:>2} ( {:>3}       {:>2} {:>3} )  ',\
-              '(   {:>3} {:>2}     {:>3}   {:>2}   {:>3}   )   {:>2} {:>3}    ',\
-              '( ( {:>3} {:>2}     {:>3} ) {:>2}   {:>3}   )   {:>2} {:>3}    ',\
-              '(   {:>3} {:>2}   ( {:>3}   {:>2}   {:>3} ) )   {:>2} {:>3}    ',\
-              '    {:>3} {:>2} (   {:>3}   {:>2}   {:>3}       {:>2} {:>3}   )',\
-              '    {:>3} {:>2} ( ( {:>3}   {:>2}   {:>3} )     {:>2} {:>3}   )',\
-              '    {:>3} {:>2} (   {:>3}   {:>2} ( {:>3}       {:>2} {:>3} ) )']
-              
-mathCoolDB = "mathCool24.txt"   # where all values are kept in a file
-math24Dict={}
+operatorDict = {1:"+", 2:"-", 3:"*", 4:"/"}
+operandList=[0] * 4
+operatorList=[''] * 3
+mathCoolDB = "mathCool24.txt"
+maxCount=2000    # Maximum Count for each run
+math24List=[]
 
-for a in cardDeck:
-    for b in cardDeck:
-        for c in cardDeck:
-            for d in cardDeck:
-                for op1 in operatorList:
-                    for op2 in operatorList:
-                        for op3 in operatorList:
-                            
-                            for precedence in operations:
-                                questionString = precedence.format(str(a),op1,str(b),op2,str(c),op3,str(d))
-                                try:    # in case evaluation fails for division-by-zero, etc.  Skip errors
-                                    answer = float(eval(questionString))
-                                    if ( answer == 24 ):
-                                        key = str(sorted([str(a),str(b),str(c),str(d)]))                                       
-                                        if key not in list(math24Dict.keys()):
-                                            #print(questionString)
-                                            math24Dict[key] = questionString
-                                        break
-                                except:
-                                    continue
+"""
+with open(mathCoolDB, "r") as mathCool24:
+    for line in mathCoolDB:
+        mathCoolDict[line] = 1
+"""
 
-mathCool24=open(mathCoolDB, "w")                                       
-for value in sorted(math24Dict.values()):
-    mathCool24.write(value + '\n')
+try:    
+    mathCool24=open(mathCoolDB, "r")
+    
+    for line in mathCool24:
+        math24List.append(line)
+        
+    mathCool24.close()
+except:
+    print(mathCoolDB, " is empty.")
+    mathCool24 = open(mathCoolDB,"w")
+    mathCool24.close()
+    
+    
+count=0
+lastCount=len(math24List)
+#maxCount += count
+
+while True:
+    for idx in range(len(operandList)):
+        operandList[idx]=randint(1,10)
+    #print(operandList)
+    
+    questionString=str(operandList[0])
+    for idx in range(len(operatorList)):
+        operatorList[idx]=operatorDict[randint(1,len(operatorDict))]
+        questionString += operatorList[idx] + str(operandList[idx+1])
+    #print(questionString)
+    answer = float(eval(questionString))
+    if answer < 0:
+        continue
+    if answer == int(answer):
+        #print("Lucky Integer:\t", questionString, "=", int(answer), ".")
+        if int(answer) == 24:
+            #print("***  Bingo 24: ", questionString, "=", int(answer), "  ***")
+            #print(questionString)
+            if questionString not in math24List:
+                
+                math24List.append(questionString)
+                count += 1
+                if count == maxCount :
+                    break
+            #break
+        
+    #else:
+        #print("Unlucky floats:\t", questionString, "=", answer, ".")
+
+mathCool24=open(mathCoolDB, "a")
+for idx in range(lastCount, len(math24List)):
+    print(math24List[idx])
+    line = math24List[idx] + '\n'
+    mathCool24.write(line)
 mathCool24.close()
-
+    
+    
