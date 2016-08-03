c4a01fae791173a7959834a4cdaff1e18e9a9425
integuru <integuru@gmail.com>
2016-08-03 14:47:33 -0400

    Update Math24.py

--- Math24.py
+++ Math24.py
@@ -1,9 +1,10 @@
 from random import randint
 from os import remove, rename
 
-operatorDict = {1:"+", 2:"-", 3:"*", 4:"/"}
-operandList=[0] * 4
-operatorList=[''] * 3
+#operatorDict = {1:"+", 2:"-", 3:"*", 4:"/"}
+#operandList=[0] * 4
+operatorList= ["+", "-", "*", "/"]
+#operatorList=[''] * 3
 mathCoolDB = "mathCool24.txt"
 maxCount=2000    # Maximum Count for each run
 math24List=[]
@@ -30,39 +31,33 @@ except:
 count=0
 lastCount=len(math24List)
 #maxCount += count
+lastCard=11
 
-while True:
-    for idx in range(len(operandList)):
-        operandList[idx]=randint(1,10)
-    #print(operandList)
-    
-    questionString=str(operandList[0])
-    for idx in range(len(operatorList)):
-        operatorList[idx]=operatorDict[randint(1,len(operatorDict))]
-        questionString += operatorList[idx] + str(operandList[idx+1])
-    #print(questionString)
-    answer = float(eval(questionString))
-    if answer < 0:
-        continue
-    if answer == int(answer):
-        #print("Lucky Integer:\t", questionString, "=", int(answer), ".")
-        if int(answer) == 24:
-            #print("***  Bingo 24: ", questionString, "=", int(answer), "  ***")
-            #print(questionString)
-            if questionString not in math24List:
-                
-                math24List.append(questionString)
-                count += 1
-                if count == maxCount :
-                    break
-            #break
-        
-    #else:
-        #print("Unlucky floats:\t", questionString, "=", answer, ".")
+for a in range(1,lastCard):
+    for b in range(1,lastCard):
+        for c in range(1,lastCard):
+            for d in range(1,lastCard):
+                for i in range(len(operatorList)):
+                    op1 = operatorList[i]
+                    for j in range(len(operatorList)):
+                        op2 = operatorList[j]
+                        for k in range(len(operatorList)):
+                            op3 = operatorList[k]
+                    
+                            questionString = str(a) + op1 + \
+                                             str(b) + op2 + \
+                                             str(c) + op3 + \
+                                             str(d)
+                                             
+                            #print(questionString)
+                            answer = float(eval(questionString))
+                            if ( answer == int(answer)) and ( int(answer) == 24):
+                                print(questionString)
+                                math24List.append(questionString)
 
 mathCool24=open(mathCoolDB, "a")
 for idx in range(lastCount, len(math24List)):
-    print(math24List[idx])
+    #print(math24List[idx])
     line = math24List[idx] + '\n'
     mathCool24.write(line)
 mathCool24.close()
